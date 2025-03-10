b791bca3952126d8cde327b692e6273ad09e6d10
JJGadgets <git@jjgadgets.tech>
2025-03-11 01:12:46 +0800

    fix(sweep): test disabling mouse keys
    slave communication has stopped working entirely

--- keyboards/ferris/sweep/keymaps/jjgadgets/config.h
+++ keyboards/ferris/sweep/keymaps/jjgadgets/config.h
@@ -28,14 +28,14 @@
 #undef MODS_TO_NEUTRALIZE
 /*#define MODS_TO_NEUTRALIZE { MOD_BIT(KC_RALT), }*/
 
-// Mouse key speed and acceleration.
-#undef MOUSEKEY_DELAY
-#define MOUSEKEY_DELAY          0
-#undef MOUSEKEY_INTERVAL
-#define MOUSEKEY_INTERVAL       16
-#undef MOUSEKEY_WHEEL_DELAY
-#define MOUSEKEY_WHEEL_DELAY    0
-#undef MOUSEKEY_MAX_SPEED
-#define MOUSEKEY_MAX_SPEED      6
-#undef MOUSEKEY_TIME_TO_MAX
-#define MOUSEKEY_TIME_TO_MAX    64
+/*// Mouse key speed and acceleration.*/
+/*#undef MOUSEKEY_DELAY*/
+/*#define MOUSEKEY_DELAY          0*/
+/*#undef MOUSEKEY_INTERVAL*/
+/*#define MOUSEKEY_INTERVAL       16*/
+/*#undef MOUSEKEY_WHEEL_DELAY*/
+/*#define MOUSEKEY_WHEEL_DELAY    0*/
+/*#undef MOUSEKEY_MAX_SPEED*/
+/*#define MOUSEKEY_MAX_SPEED      6*/
+/*#undef MOUSEKEY_TIME_TO_MAX*/
+/*#define MOUSEKEY_TIME_TO_MAX    64*/
