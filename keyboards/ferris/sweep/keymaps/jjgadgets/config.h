7825e1519205fd18e8c74eca901fe4f77c31df5b
JJGadgets <git@jjgadgets.tech>
2025-03-10 18:33:25 +0800

    feat(sweep): mouse keys, cleanup

--- keyboards/ferris/sweep/keymaps/jjgadgets/config.h
+++ keyboards/ferris/sweep/keymaps/jjgadgets/config.h
@@ -28,21 +28,14 @@
 #undef MODS_TO_NEUTRALIZE
 /*#define MODS_TO_NEUTRALIZE { MOD_BIT(KC_RALT), }*/
 
-/*// Mouse key speed and acceleration.*/
-/*#undef MOUSEKEY_DELAY*/
-/*#define MOUSEKEY_DELAY          0*/
-/*#undef MOUSEKEY_INTERVAL*/
-/*#define MOUSEKEY_INTERVAL       16*/
-/*#undef MOUSEKEY_WHEEL_DELAY*/
-/*#define MOUSEKEY_WHEEL_DELAY    0*/
-/*#undef MOUSEKEY_MAX_SPEED*/
-/*#define MOUSEKEY_MAX_SPEED      6*/
-/*#undef MOUSEKEY_TIME_TO_MAX*/
-/*#define MOUSEKEY_TIME_TO_MAX    64*/
-/**/
-/*// Thumb Combos*/
-/*#if defined (MIRYOKU_KLUDGE_THUMBCOMBOS)*/
-/*  #define COMBO_COUNT 8*/
-/*  #define COMBO_TERM 200*/
-/*  #define EXTRA_SHORT_COMBOS*/
-/*#endif*/
+// Mouse key speed and acceleration.
+#undef MOUSEKEY_DELAY
+#define MOUSEKEY_DELAY          0
+#undef MOUSEKEY_INTERVAL
+#define MOUSEKEY_INTERVAL       16
+#undef MOUSEKEY_WHEEL_DELAY
+#define MOUSEKEY_WHEEL_DELAY    0
+#undef MOUSEKEY_MAX_SPEED
+#define MOUSEKEY_MAX_SPEED      6
+#undef MOUSEKEY_TIME_TO_MAX
+#define MOUSEKEY_TIME_TO_MAX    64
