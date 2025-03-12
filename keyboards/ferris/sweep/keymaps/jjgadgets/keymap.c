a19c1d76654a6c6cf2f69bc93ef6deb5d523b724
JJGadgets <git@jjgadgets.tech>
2025-03-12 20:41:45 +0800

    feat(sweep): test OSM shift, pnohty combos & numpad+, fix repeat key & layer lock tap

--- keyboards/ferris/sweep/keymaps/jjgadgets/keymap.c
+++ keyboards/ferris/sweep/keymaps/jjgadgets/keymap.c
@@ -2,6 +2,7 @@
 #include "action.h"
 #include "keycodes.h"
 #include "modifiers.h"
+#include "process_key_override.h"
 #include "quantum.h"
 #include "quantum_keycodes.h"
 #include QMK_KEYBOARD_H
@@ -19,17 +20,17 @@ enum layer_names {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_DEFAULT] = LAYOUT_split_3x5_2(
         KC_Q, KC_W, KC_F, KC_P, KC_G,                           KC_J, KC_L, KC_U, KC_Y, KC_COLON,
-        KC_A, KC_R, KC_S, KC_T, KC_D,                           KC_H, KC_N, KC_E, KC_I, KC_O,
-        LT(_FN, KC_Z), KC_X, RALT_T(KC_C), RGUI_T(KC_V), KC_B,  KC_K, LGUI_T(KC_M), LALT_T(KC_COMM), RCTL_T(KC_DOT), LT(_MOUSE, KC_SLSH),
+        KC_A, KC_R, KC_S, LSFT_T(KC_T), KC_D,                   KC_H, LSFT_T(KC_N), KC_E, KC_I, KC_O,
+        LT(_FN, KC_Z), KC_X, RALT_T(KC_C), RGUI_T(KC_V), KC_B,  KC_K, LGUI_T(KC_M), LALT_T(KC_COMM), RCTL_T(KC_DOT), LT(_MOUSE, KC_SLASH),
         LCTL_T(KC_ESC), LSFT_T(KC_SPC),                         MEH_T(KC_BSPC), LT(_NUM, QK_REP)
         /*LCTL_T(KC_SPC), LSFT_T(OSM(MOD_LSFT)),                  MEH_T(KC_BSPC), LT(_NUM, QK_REP)*/
         /*LCTL_T(QK_GESC), LSFT_T(OSM(MOD_LSFT)),                         MEH_T(KC_SPC), LT(_NUM, KC_BSPC)*/ // one day I'll get used to this
     ),
     [_NUM] = LAYOUT_split_3x5_2(
-        KC_TILDE, KC_1, KC_2, KC_3, KC_COMM,                    KC_BSLS, LT(_NUM, KC_HOME), KC_UP, LT(_NUM, KC_END), KC_DEL,
-        QK_GESC, KC_4, KC_5, KC_6, KC_DOT,                      KC_TAB, KC_LEFT, KC_DOWN, KC_RIGHT, KC_QUOTE,
-        KC_ENTER, KC_7, KC_8, KC_9, KC_0,                       KC_LBRC, KC_RBRC, KC_MINUS, KC_EQUAL, KC_PSCR,
-        LCTL_T(TG(_NUM)), LSFT_T(KC_SPC),                       MEH_T(KC_BSPC), KC_TRNS
+        KC_TILDE, KC_4, KC_5, KC_6, KC_COMM,                    KC_BSLS, LT(_NUM, KC_HOME), KC_UP, LT(_NUM, KC_END), KC_DEL,
+        KC_0, KC_1, KC_2, KC_3, KC_DOT,                         KC_TAB, KC_LEFT, KC_DOWN, KC_RIGHT, KC_QUOTE,
+        KC_ENTER, KC_7, KC_8, KC_9, KC_SLASH,                   KC_LBRC, KC_RBRC, KC_MINUS, KC_EQUAL, KC_PSCR,
+        LCTL_T(QK_LLCK), LSFT_T(KC_SPC),                        MEH_T(KC_BSPC), KC_TRNS
     ),
     [_FN] = LAYOUT_split_3x5_2(
         KC_LALT, KC_F1, KC_F2, KC_F3, KC_F10,                   KC_NO, KC_NO, KC_NO, KC_NO, LCA(KC_DEL),
@@ -47,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 // custom hold-tap bindings
 bool jj_tap_hold_override(keyrecord_t *therecord, bool tap_override, uint16_t tapkey, bool hold_override, uint16_t holdkey) {
-    if (!therecord->tap.count && therecord->event.pressed && tap_override) {
+    if (therecord->tap.count && therecord->event.pressed && tap_override) {
         tap_code16(tapkey);
         return false;
     } else if (!therecord->tap.count && therecord->event.pressed && hold_override) {
@@ -62,10 +63,21 @@ bool jj_tap_hold_override(keyrecord_t *therecord, bool tap_override, uint16_t ta
 };
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-        case LT(_NUM, QK_REP): return jj_tap_hold_override(record, true, QK_REP, false, KC_NO); break;
-        case LCTL_T(TG(_NUM)): return jj_tap_hold_override(record, true, TG(_NUM), false, KC_NO); break;
         case LT(_NUM, KC_HOME): return jj_tap_hold_override(record, false, KC_NO, true, KC_PGUP); break;
         case LT(_NUM, KC_END): return jj_tap_hold_override(record, false, KC_NO, true, KC_PGDN); break;
+        case LSFT_T(KC_T):
+        case LSFT_T(KC_N):
+            return jj_tap_hold_override(record, false, KC_NO, true, OSM(MOD_LSFT)); break;
+        case LT(_NUM, QK_REP):
+            if (record->tap.count && record->event.pressed) {
+                repeat_key_invoke(&record->event);
+                return false; break;
+            }
+        case LCTL_T(QK_LLCK):
+            if (record->tap.count && record->event.pressed) {
+                layer_lock_invert(get_highest_layer(layer_state));
+                return false; break;
+            }
     }
     return true;
 };
@@ -75,12 +87,20 @@ const uint16_t PROGMEM combo_enter[] = {KC_N, KC_E, KC_I, COMBO_END};
 const uint16_t PROGMEM combo_mouse_default[] = {KC_L, KC_U, KC_Y, COMBO_END};
 const uint16_t PROGMEM combo_mouse_layered[] = {MS_WHLL, MS_UP, MS_WHLR, COMBO_END};
 const uint16_t PROGMEM combo_caps[] = {KC_SPC, KC_BSPC, COMBO_END};
+const uint16_t PROGMEM combo_caps2[] = {LSFT_T(KC_SPC), MEH_T(KC_BSPC), COMBO_END};
+const uint16_t PROGMEM combo_tab[] = {KC_X, KC_C, COMBO_END};
+const uint16_t PROGMEM combo_one_shot_shift_l[] = {KC_W, KC_F, COMBO_END};
+const uint16_t PROGMEM combo_one_shot_shift_r[] = {KC_U, KC_Y, COMBO_END};
 // This globally defines all combos to be used
 combo_t key_combos[] = {
     COMBO(combo_enter, KC_ENTER),
     COMBO(combo_mouse_default, TG(_MOUSE)),
     COMBO(combo_mouse_layered, TG(_MOUSE)),
     COMBO(combo_caps, QK_CAPS_WORD_TOGGLE),
+    COMBO(combo_caps2, QK_CAPS_WORD_TOGGLE),
+    COMBO(combo_tab, KC_TAB),
+    COMBO(combo_one_shot_shift_l, OSM(MOD_LSFT)),
+    COMBO(combo_one_shot_shift_r, OSM(MOD_LSFT)),
 };
 
 // key overrides (mod morphs)
@@ -88,12 +108,14 @@ const key_override_t alt_tab_morph = ko_make_with_layers(MOD_BIT(KC_RALT), KC_A,
 const key_override_t alt_tab_shift_morph = ko_make_with_layers(MOD_BIT(KC_RALT) | MOD_BIT(KC_LSFT), KC_A, LSFT(RALT(KC_TAB)), 1 << _DEFAULT);
 const key_override_t colon_morph = ko_make_basic(MOD_MASK_SHIFT, KC_COLON, KC_SCLN);
 const key_override_t tilde_morph = ko_make_basic(MOD_MASK_SHIFT, KC_TILDE, KC_GRV);
+const key_override_t right_bracket_morph = ko_make_with_layers(MOD_MASK_SHIFT, KC_SLASH, KC_RIGHT_PAREN, 1 << _NUM);
 // This globally defines all key overrides to be used
 const key_override_t *key_overrides[] = {
 	&alt_tab_morph,
 	&alt_tab_shift_morph,
     &colon_morph,
     &tilde_morph,
+    &right_bracket_morph,
 };
 
 // hold-tap config
